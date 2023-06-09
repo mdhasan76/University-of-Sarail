b0a13f2af9f562e6e070ef04279ca195d21f16f5
Md Hasan <mdhasan8064@gmail.com>
2023-06-09 07:53:27 +0600

    Done project setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// parser or Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req: Request, res: Response) =>{
+    res.send(`Hello, You server is running`)
+})
+
+export default app;
