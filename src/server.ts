70053cfe433a4168427350cae42509581d5153ba
Md Hasan <mdhasan8064@gmail.com>
2023-06-13 19:56:21 +0600

    check huskey

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
-main().catch(err=> console.log(err));
+main().catch(err => console.log(err))
 
-async function main(){
-    try{
-        await mongoose.connect(config.database_url  as string)
-        console.log("Server is connected succeflly");
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Server is connected succeflly')
 
-        app.listen(config.port, () =>{
-            console.log("server is running on the ", config.port);
-        })
-    }
-    catch(err){
-        console.log("Server not connected",err);
-    }
+    app.listen(config.port, () => {
+      console.log('server is running on the ', config.port)
+    })
+  } catch (err) {
+    console.log('Server not connected', err)
+  }
 }
