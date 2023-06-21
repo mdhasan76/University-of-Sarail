3c9984e6b94eb3b69ce49c0c45b73f9deaba1af4
Md Hasan <mdhasan8064@gmail.com>
2023-06-21 10:16:30 +0600

    12-7

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,26 +1,61 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
 import path from 'path'
+const { combine, timestamp, label, printf, prettyPrint } = format
 
-const logger = winston.createLogger({
+//Custom log formate
+const myFormat = printf(({ level, message, label }) => {
+  const date = new Date()
+  const hour = date.getHours()
+  const minuits = date.getMinutes()
+  const seconds = date.getSeconds()
+
+  return `${date.toDateString()} ${hour}:${minuits}:${seconds} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successs',
+        'UA-%DATE%success.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
 
-const errorLogger = winston.createLogger({
+const errorLogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-      level: 'error',
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'UA-%DATE%error.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
